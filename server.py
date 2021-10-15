fae663c87809af5f6b102f969548930d6d185a94
ernestchu <b073040018@nsysu.edu.tw>
2021-10-15 19:32:08 +0000

    update

--- server.py
+++ server.py
@@ -2,10 +2,12 @@ import os, sys
 sys.path.append('tsm-net')
 from tsmnet import Stretcher
 import torchaudio
+import soundfile as sf
 from flask import Flask, flash, request, redirect, url_for, send_from_directory
 from werkzeug.utils import secure_filename
 
-UPLOAD_FOLDER = 'tmp'
+UPLOAD_FOLDER = '.tmp'
+os.makedirs(UPLOAD_FOLDER, exist_ok=True)
 ALLOWED_EXTENSIONS = {'wav', 'mp3'}
 
 stretcher = Stretcher('tsm-net/weights')
@@ -16,6 +18,16 @@ app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 def allowed_file(filename):
     return '.' in filename and \
            filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+def stretch(dirs, filename, rate):
+    x, sr = torchaudio.load(os.path.join(dirs, filename))
+    x = torchaudio.transforms.Resample(orig_freq=sr, new_freq=22050)(x)
+    sr = 22050
+    filename = filename.split('.')
+    filename[-2] += '-stretched'
+    filename = '.'.join(filename)
+    sf.write(os.path.join(dirs, filename), stretcher(x, rate).T, sr)
+    return filename
 
 @app.route('/', methods=['GET', 'POST'])
 def upload_file():
@@ -33,12 +45,18 @@ def upload_file():
         if file and allowed_file(file.filename):
             filename = secure_filename(file.filename)
             file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            filename = stretch(
+                app.config['UPLOAD_FOLDER'], filename,
+                float(request.form['rate'])
+            )
             return redirect(url_for('download_file', name=filename))
     return '''
     <!doctype html>
     <title>Upload new File</title>
     <h1>Upload new File</h1>
     <form method=post enctype=multipart/form-data>
+      <label for="rate">rate</label>
+      <input id="rate" name="rate" type="number" step="any" min="0.01" value="0.75">
       <input type=file name=file>
       <input type=submit value=Upload>
     </form>
